6c5f6982f2ccca52b767c67fc848ed670a8dd13f
yusa-a <yusa-a@weseek.co.jp>
2026-03-20 23:51:00 +0900

    fix norm

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,22 +6,22 @@
 /*   By: ayusa <ayusa@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 23:18:36 by ayusa             #+#    #+#             */
-/*   Updated: 2026/03/20 23:18:46 by ayusa            ###   ########.fr       */
+/*   Updated: 2026/03/20 23:50:18 by ayusa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strcmp(const char *s1, const char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    while (s1[i] && s2[i])
-    {
-        if ((unsigned char)s1[i] != (unsigned char)s2[i])
-            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-        i++;
-    }
-    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
