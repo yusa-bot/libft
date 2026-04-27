c2d8461498488b09b69f30b7eaef7143b6cf425d
yusa-a <yusaaihara3@gmail.com>
2026-04-27 15:48:16 +0900

    fix: norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ayusa <ayusa@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 18:02:48 by ayusa             #+#    #+#             */
-/*   Updated: 2026/04/16 18:03:00 by ayusa            ###   ########.fr       */
+/*   Updated: 2026/04/27 15:40:48 by ayusa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,15 @@ static char	*read_until_nl(int fd, char *stash)
 		stash = ft_strdup("");
 	if (!stash)
 		return (NULL);
-
 	bytes = 1;
 	while (!ft_strchr(stash, '\n') && bytes > 0)
 	{
 		bytes = read(fd, buf, BUFFER_SIZE);
 		if (bytes < 0)
-		{
-			free(stash);
-			return (NULL);
-		}
+			return (free(stash), NULL);
 		buf[bytes] = '\0';
-
 		tmp = ft_strjoin(stash, buf);
-
 		free(stash);
-
 		stash = tmp;
 		if (!stash)
 			return (NULL);
@@ -52,13 +45,11 @@ static char	*extract_line(const char *stash)
 
 	if (!stash || stash[0] == '\0')
 		return (NULL);
-
 	nl = ft_strchr(stash, '\n');
 	if (nl)
 		len = (size_t)(nl - stash) + 1;
 	else
 		len = ft_strlen(stash);
-
 	return (ft_substr(stash, 0, len));
 }
 
@@ -69,17 +60,14 @@ static char	*update_stash(char *stash)
 
 	if (!stash)
 		return (NULL);
-
 	nl = ft_strchr(stash, '\n');
 	if (!nl)
 	{
 		free(stash);
 		return (NULL);
 	}
-
 	new_stash = ft_strdup(nl + 1);
 	free(stash);
-
 	if (!new_stash || new_stash[0] == '\0')
 	{
 		free(new_stash);
@@ -95,11 +83,9 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
 	stash = read_until_nl(fd, stash);
 	if (!stash)
 		return (NULL);
-
 	line = extract_line(stash);
 	if (!line)
 	{
@@ -107,7 +93,6 @@ char	*get_next_line(int fd)
 		stash = NULL;
 		return (NULL);
 	}
-
 	stash = update_stash(stash);
 	return (line);
 }
